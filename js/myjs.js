4050528cb2ea8a6d115659058b833599e64701d2
Debajit-webdev <debajitchakrabortyolw960@gmail.com>
2022-03-05 19:26:59 +0530

    caraousel add

--- js/myjs.js
+++ js/myjs.js
@@ -8,267 +8,129 @@ $(".hamburger").click(function(){
 
 //  CART HOVER
 
-$(".p-click-1").mouseenter(function(){
-    $(".p-txt-hide-1").fadeOut(300);
-    $(".cart-1").fadeIn(300);
-}); 
+
 $(".cart-1").click(function(){
     if(alert('Monoprice 10145 5.1 - Home Theater Speaker -- Added To Cart ')){}
     else window.location.reload(); 
-}); 
-$(".p-click-1").mouseleave(function(){
-    $(".p-txt-hide-1").fadeIn(300);
-    $(".cart-1").fadeOut(300);
-});
-
-
-$(".p-click-2").mouseenter(function(){
-    $(".p-txt-hide-2").fadeOut(300);
-    $(".cart-2").fadeIn(300);
 }); 
 $(".cart-2").click(function(){
     if(alert('Samsung Galaxy S III (S3) -- Added To Cart ')){}
     else window.location.reload(); 
 }); 
-$(".p-click-2").mouseleave(function(){
-    $(".p-txt-hide-2").fadeIn(300);
-    $(".cart-2").fadeOut(300);
-}); 
-
-
-$(".p-click-3").mouseenter(function(){
-    $(".p-txt-hide-3").fadeOut(300);
-    $(".cart-3").fadeIn(300);
-}); 
 $(".cart-3").click(function(){
     if(alert('Photos of Us Collage -- Added To Cart ')){}
     else window.location.reload(); 
-}); 
-$(".p-click-3").mouseleave(function(){
-    $(".p-txt-hide-3").fadeIn(300);
-    $(".cart-3").fadeOut(300);
-}); 
-
-
-$(".p-click-4").mouseenter(function(){
-    $(".p-txt-hide-4").fadeOut(300);
-    $(".cart-4").fadeIn(300);
 }); 
 $(".cart-4").click(function(){
     if(alert('Smart Accessories -- Added To Cart ')){}
     else window.location.reload(); 
 }); 
-$(".p-click-4").mouseleave(function(){
-    $(".p-txt-hide-4").fadeIn(300);
-    $(".cart-4").fadeOut(300);
-}); 
-
-
-$(".p-click-5").mouseenter(function(){
-    $(".p-txt-hide-5").fadeOut(300);
-    $(".cart-5").fadeIn(300);
-}); 
 $(".cart-5").click(function(){
     if(alert('Photos of Us Collage -- Added To Cart ')){}
     else window.location.reload(); 
-}); 
-$(".p-click-5").mouseleave(function(){
-    $(".p-txt-hide-5").fadeIn(300);
-    $(".cart-5").fadeOut(300);
-}); 
-
-
-$(".p-click-6").mouseenter(function(){
-    $(".p-txt-hide-6").fadeOut(300);
-    $(".cart-6").fadeIn(300);
 }); 
 $(".cart-6").click(function(){
     if(alert('Smart Watch -- Added To Cart ')){}
     else window.location.reload(); 
 }); 
-$(".p-click-6").mouseleave(function(){
-    $(".p-txt-hide-6").fadeIn(300);
-    $(".cart-6").fadeOut(300);
-});
-
-
-$(".p-click-7").mouseenter(function(){
-    $(".p-txt-hide-7").fadeOut(300);
-    $(".cart-7").fadeIn(300);
-}); 
 $(".cart-7").click(function(){
     if(alert('Wireless Audio Soundbar -- Added To Cart ')){}
     else window.location.reload(); 
-}); 
-$(".p-click-7").mouseleave(function(){
-    $(".p-txt-hide-7").fadeIn(300);
-    $(".cart-7").fadeOut(300);
-});
-
-
-$(".p-click-8").mouseenter(function(){
-    $(".p-txt-hide-8").fadeOut(300);
-    $(".cart-8").fadeIn(300);
 }); 
 $(".cart-8").click(function(){
     if(alert('Headphone -- Added To Cart ')){}
     else window.location.reload(); 
 }); 
-$(".p-click-8").mouseleave(function(){
-    $(".p-txt-hide-8").fadeIn(300);
-    $(".cart-8").fadeOut(300);
-});
-
-
-$(".p-click-9").mouseenter(function(){
-    $(".p-txt-hide-9").fadeOut(300);
-    $(".cart-9").fadeIn(300);
-}); 
 $(".cart-9").click(function(){
     if(alert('Harman Kardon BDS 870 -- Added To Cart ')){}
     else window.location.reload(); 
-}); 
-$(".p-click-9").mouseleave(function(){
-    $(".p-txt-hide-9").fadeIn(300);
-    $(".cart-9").fadeOut(300);
-});
-
-
-$(".p-click-10").mouseenter(function(){
-    $(".p-txt-hide-10").fadeOut(300);
-    $(".cart-10").fadeIn(300);
 }); 
 $(".cart-10").click(function(){
     if(alert('Harman Kardon BDS 870 -- Added To Cart ')){}
     else window.location.reload(); 
 }); 
-$(".p-click-10").mouseleave(function(){
-    $(".p-txt-hide-10").fadeIn(300);
-    $(".cart-10").fadeOut(300);
-});
-
-
-$(".p-click-11").mouseenter(function(){
-    $(".p-txt-hide-11").fadeOut(300);
-    $(".cart-11").fadeIn(300);
-}); 
 $(".cart-11").click(function(){
     if(alert('Samsung Galaxy S III (S3) -- Added To Cart ')){}
     else window.location.reload(); 
-}); 
-$(".p-click-11").mouseleave(function(){
-    $(".p-txt-hide-11").fadeIn(300);
-    $(".cart-11").fadeOut(300);
-});
-
-
-$(".p-click-12").mouseenter(function(){
-    $(".p-txt-hide-12").fadeOut(300);
-    $(".cart-12").fadeIn(300);
 }); 
 $(".cart-12").click(function(){
     if(alert('Smart Watch -- Added To Cart ')){}
     else window.location.reload(); 
 }); 
-$(".p-click-12").mouseleave(function(){
-    $(".p-txt-hide-12").fadeIn(300);
-    $(".cart-12").fadeOut(300);
-});
-
-
-$(".p-click-13").mouseenter(function(){
-    $(".p-txt-hide-13").fadeOut(300);
-    $(".cart-13").fadeIn(300);
-}); 
 $(".cart-13").click(function(){
     if(alert('Samsung UN28H400 28 - inch LED Monitor -- Added To Cart ')){}
     else window.location.reload(); 
-}); 
-$(".p-click-13").mouseleave(function(){
-    $(".p-txt-hide-13").fadeIn(300);
-    $(".cart-13").fadeOut(300);
 });
-
-
-$(".p-click-14").mouseenter(function(){
-    $(".p-txt-hide-14").fadeOut(300);
-    $(".cart-14").fadeIn(300);
-}); 
 $(".cart-14").click(function(){
     if(alert('Asus Zenphone 6 -- Added To Cart ')){}
     else window.location.reload(); 
-}); 
-$(".p-click-14").mouseleave(function(){
-    $(".p-txt-hide-14").fadeIn(300);
-    $(".cart-14").fadeOut(300);
-});
-
-
-$(".p-click-15").mouseenter(function(){
-    $(".p-txt-hide-15").fadeOut(300);
-    $(".cart-15").fadeIn(300);
 }); 
 $(".cart-15").click(function(){
     if(alert('Logitech Keyboard -- Added To Cart ')){}
     else window.location.reload(); 
 }); 
-$(".p-click-15").mouseleave(function(){
-    $(".p-txt-hide-15").fadeIn(300);
-    $(".cart-15").fadeOut(300);
-});
-
-
-$(".p-click-16").mouseenter(function(){
-    $(".p-txt-hide-16").fadeOut(300);
-    $(".cart-16").fadeIn(300);
-}); 
 $(".cart-16").click(function(){
     if(alert('Wireless Audio Soundbar -- Added To Cart ')){}
     else window.location.reload(); 
-}); 
-$(".p-click-16").mouseleave(function(){
-    $(".p-txt-hide-16").fadeIn(300);
-    $(".cart-16").fadeOut(300);
-});
-
-
-$(".p-click-17").mouseenter(function(){
-    $(".p-txt-hide-17").fadeOut(300);
-    $(".cart-17").fadeIn(300);
 }); 
 $(".cart-17").click(function(){
     if(alert('Audio Soundbar -- Added To Cart ')){}
     else window.location.reload(); 
 }); 
-$(".p-click-17").mouseleave(function(){
-    $(".p-txt-hide-17").fadeIn(300);
-    $(".cart-17").fadeOut(300);
-});
-
-
-$(".p-click-18").mouseenter(function(){
-    $(".p-txt-hide-18").fadeOut(300);
-    $(".cart-18").fadeIn(300);
-}); 
 $(".cart-18").click(function(){
     if(alert('Asus Zenphone 6 -- Added To Cart ')){}
     else window.location.reload(); 
-}); 
-$(".p-click-18").mouseleave(function(){
-    $(".p-txt-hide-18").fadeIn(300);
-    $(".cart-18").fadeOut(300);
-});
-
-
-$(".p-click-19").mouseenter(function(){
-    $(".p-txt-hide-19").fadeOut(300);
-    $(".cart-19").fadeIn(300);
 }); 
 $(".cart-19").click(function(){
     if(alert('TP-Link Wireless Router -- Added To Cart ')){}
     else window.location.reload(); 
 }); 
-$(".p-click-19").mouseleave(function(){
-    $(".p-txt-hide-19").fadeIn(300);
-    $(".cart-19").fadeOut(300);
+
+// PRODUCT CAROUSEL
+$(".p-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    autoplay: true,
+    autoplaySpeed: 600,
+    responsive:{
+        0:{
+            items: 1,
+        },
+        461:{
+            items: 2
+        },
+        586:{
+            items: 3
+        },
+        831:{
+            items: 4
+        },
+        1071:{
+            items: 5
+        }
+    }
+});
+
+// COMPANY CAROUSEL
+$(".c-carousel").owlCarousel({
+    items: 5,
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 600,
+    responsive:{
+        0:{
+            items: 2,
+        },
+        461:{
+            items: 3,
+        },
+        751:{
+            items: 4,
+        },
+        916:{
+            items: 5,            
+        }
+    }
 });
